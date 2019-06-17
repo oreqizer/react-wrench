f8b204fd5389d148eb9521a7dfc07012bd27599b
oreqizer <oreqizer@gmail.com>
2019-06-17 15:00:31 +0200

    fix(components): ClientOnly return type signature

--- src/components/ClientOnly/index.tsx
+++ src/components/ClientOnly/index.tsx
@@ -5,7 +5,7 @@ type Props = {
   loader?: React.ReactNode;
 };
 
-const ClientOnly = ({ children, loader = null }: Props) => {
+const ClientOnly = ({ children, loader = null }: Props): React.ReactNode => {
   const [mounted, setMounted] = React.useState(false);
 
   React.useEffect(() => {
